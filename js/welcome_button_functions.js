3ef9f58f509313ee755d4c52c5236c15b83fd8df
Smart-Machine <radu.calin500@gmail.com>
2022-07-30 22:16:30 +0300

    adding video

--- js/welcome_button_functions.js
+++ js/welcome_button_functions.js
@@ -1,3 +1,30 @@
 function welcome_button_click() {
     window.scrollBy(0, 950);
 }
+
+function foo() {
+    var content = document.getElementById('');
+
+    function moveDown () {
+        var top = parseInt(content.style.marginTop); // get the top margin
+                                                     // we'll be using this to
+                                                     // push the div down
+
+        if (!top) {
+            top = 0; // if the margin is undefined, default it to zero
+        }
+
+        top += 1; // add 20 pixels to the current margin
+
+        content.style.marginTop = top + 'px'; // push div down
+
+        if (top < 600) {
+            // If it's not yet 200 pixels then call this function
+            // again in another 100 milliseconds (100 ms gives us
+            // roughly 10 fps which should be good enough):
+            setTimeout(moveDown, 5);
+        }
+    }
+    moveDown()
+}
+
--- js/welcome_scroll.js
+++ js/welcome_scroll.js
@@ -1,11 +1,34 @@
-function sleep(miliseconds) {
-    var currentTime = new Date().getTime();
+var blur_video = document.getElementById('welcome_blur_video');
 
-    while (currentTime + miliseconds >= new Date().getTime()) {
-    }
+function sleep(time) {
+  return new Promise(resolve=>setTimeout(resolve, time));
 }
 
-sleep(600);
+function scroll() {
+    window.scrollBy(0, 500);
+}
 
-window.scrollBy(0, 500);
+function enable_video() {
+    var video = document.getElementById('welcome_video');
+    video.style.visibility = "visible";
+}
+
+function unblur() {
+    current_opacity = window.getComputedStyle(blur_video).getPropertyValue('opacity');
+    opacity = current_opacity - 0.001;
+    blur_video.style.opacity = (opacity * 100) + '%';
+}
+
+async function unblur_video() {
+    current_opacity = window.getComputedStyle(blur_video).getPropertyValue('opacity');
+    while (current_opacity > 0.75) {
+        unblur();
+        await sleep(100);
+    } 
+}
+
+window.setTimeout(scroll, 1000);
+window.setTimeout(enable_video, 2000);
+unblur_video();
+
 
